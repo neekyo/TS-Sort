efa225a761b32bd43dafbd539d8258dbf42b4fd8
Nick Sherman <ogleffeckt2@yahoo.com>
2020-06-29 15:42:18 -0400

    re-factored to use abstract class & inheritance instead of an interface

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([ 10, 3, -5, 0 ]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+// NumbersCollection
+const numbersCollection = new NumbersCollection([ 10, 3, -5, 0 ]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
+// CharactersCollection
 const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+// LinkedList
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
